41dcaa466b62346c77bb49a44ded55d3c0e8604d
yassmineayari <yassmineayari26@gmail.com>
2025-03-13 09:40:51 +0100

    commit ex 15

--- projet.py
+++ projet.py
@@ -4,5 +4,10 @@ def somme(T):
       S+=t
       return S
 Data=[1,3,5]
-Som =sum(Data)
-print("la somme est : ",Som)
+if Data:
+  print("la somme est : ",sum(Data))
+  print("le min est : ",min(Data))
+  print("le max est : ",max(Data))
+else:
+  print("dossier vide")
+
